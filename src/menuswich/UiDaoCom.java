46e5749a431adac323510fe79d9bb668efc000f5
root <root@DESKTOP-R94IB3A.localdomain>
2019-02-24 14:41:18 +0100

    added close method for the entitymanagerfactory

--- src/menuswich/UiDaoCom.java
+++ src/menuswich/UiDaoCom.java
@@ -1,23 +1,22 @@
-
 package menuswich;
+
 import dao.ProductDAOImpl;
 import run.JPACRUD;
 import ui.UI;
+
 public class UiDaoCom {
-    
-    
-  //  
-    
-    public static void DBUIConnection(int i){
-       ProductDAOImpl db = new ProductDAOImpl();
-       UI ui = new UI();
+
+    //
+    public static void DBUIConnection(int i) {
+        ProductDAOImpl db = new ProductDAOImpl();
+        UI ui = new UI();
         boolean loop = true;
-       
-        while (loop){
-            switch (i){
-                case 1: 
+
+        while (loop) {
+            switch (i) {
+                case 1:
                     db.create(ui.enterName(), ui.enterPrice());
-                    
+
                     break;
                 case 2:
                     ui.printList(db.getAll());
@@ -25,35 +24,33 @@ public class UiDaoCom {
                 case 3:
                     ui.printList(db.getByName(ui.enterName()));
                     break;
-                case 4: 
+                case 4:
                     ui.printList(db.getByPriceRange(ui.enterPrice(), ui.enterPrice()));
                     break;
                 case 5:
-                   
+
                     db.updateName(ui.enterId(), ui.enterName());
-                    
+
                     break;
                 case 6:
                     db.updatePrice(ui.enterId(), ui.enterPrice());
-                    
+
                     break;
-                case 7: 
+                case 7:
                     db.remove(ui.enterId());
-                    
+
                     break;
                 case 0:
-                    //lägg in en metod som skapats i daoimpl klassen som 
-                    //avslutar emf i en try catch. dvs. att den ska avsluta den 
-                    //enbart om det finns en dvs emf !=null.  
+
                     loop = false;
-                    JPACRUD.loop=false;
-                    
+                    JPACRUD.loop = false;
+
                     break;
                 default:
                     DBUIConnection(ui.menu());
                     break;
             }
-            loop=false;
+            loop = false;
         }
     }
 }
--- src/run/JPACRUD.java
+++ src/run/JPACRUD.java
@@ -3,14 +3,19 @@ package run;
 import ui.UI;
 import menuswich.UiDaoCom;
 //
+
 public class JPACRUD {
 
     public static boolean loop = true;
 
     public static void main(String[] args) {
-        
-        while (loop){
-        UiDaoCom.DBUIConnection(UI.menu());
+
+        while (loop) {
+            UiDaoCom.DBUIConnection(UI.menu());
+        }
+
+        if (dao.ProductDAOImpl.emf != null) {
+            dao.ProductDAOImpl.emf.close();
         }
     }
 
